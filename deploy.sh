289bbea77f93ddfdf32ddeba9a1f5be12ff9ede9
Cory Gugler <cory.gugler@gmail.com>
2014-06-10 22:18:47 -0400

    DB sync, rough completion

--- deploy.sh
+++ deploy.sh
@@ -111,14 +111,24 @@ function deploy_database() {
             backup_remote_db
             eval mysqldump -u \${$ARG1[db_user]} -p\${$ARG1[db_password]} \${$ARG1[db_name]} | ssh \${$ARG2[user_name]}@\${$ARG2[server_name]} \"mysql -u \${$ARG2[db_user]} -p\${$ARG2[db_password]} -h \${$ARG2[sql_host]} \${$ARG2[db_name]}\"
             check_for_search_replace
-            # scp ./Search-Replace-DB-master/searchreplacedb2cli.php ./Search-Replace-DB-master/searchreplacedb2.php $PRODUSER@$PRODSERVER:$PRODDIR
-            # echo "Running Search replace on production DB"
-            # # Heredoc to run multiple commands
-            # ssh $PRODUSER@$PRODSERVER <<SYNCITREMOTE
-            # $PRODDIR$SEARCHREPLACE -h $PRODSQLHOST -u $PRODDBUSER -p $PRODDBPASS -d $PRODDBNAME -c utf8 -s "$DEVSERVER" -r "$PRODSERVER"
-            # rm $PRODDIR$SEARCHREPLACE
-            # rm $PRODDIR$SEARCHREPLACEINC
-            # SYNCITREMOTE
+            scp ./Search-Replace-DB-master/srdb.cli.php ./Search-Replace-DB-master/srdb.class.php \${$ARG2[user_name]}@\${$ARG2[server_name]}:\${$ARG2[directory]}
+            eval echo "Running Search replace on \${$ARG2[server_name]}"
+            eval local remote_user=\${$ARG2[user_name]}
+            eval local remote_server=\${$ARG2[server_name]}
+            eval local remote_dir=\${$ARG2[directory]}
+            eval local search_replace="Search-Replace-DB-master/srdb.cli.php"
+            eval local search_replace_inc="Search-Replace-DB-master/srdb.class.php"
+            eval local remote_sql_host=\${$ARG2[sql_host]}
+            eval local remote_db_user=\${$ARG2[db_user]}
+            eval local remote_db_pass=\${$ARG2[db_password]}
+            eval local remote_db_name=\${$ARG2[db_name]}
+            eval local local_server=\${$ARG1[server_name]}
+
+            # Send a heredoc
+            ssh $remote_user@$remote_server <<-EOF
+				$remote_dir$search_replace -h $remote_sql_host -u $remote_db_user -p $remote_db_pass -n $remote_db_name -s "$local_server" -r "$remote_server"
+				rm $remote_dir$search_replace ; rm $remote_dir$search_replace_inc
+			EOF
         fi
     elif [ $temp2 == "Y" ] ; then
         # second arg is the local environment
@@ -126,6 +136,30 @@ function deploy_database() {
             backup_local_db
         else
             backup_local_db
+
+            eval local remote_user=\${$ARG1[user_name]}
+            eval local remote_server=\${$ARG1[server_name]}
+            eval local remote_sql_host=\${$ARG1[sql_host]}
+            eval local remote_db_user=\${$ARG1[db_user]}
+            eval local remote_db_pass=\${$ARG1[db_password]}
+            eval local remote_db_name=\${$ARG1[db_name]}
+            eval local backup_file_name=./sql/$remote_server-$(date +%Y%m%d_%H%M).sql
+            eval local local_sql_host=\${$ARG2[sql_host]}
+            eval local local_dir=\${$ARG2[directory]}
+            eval local local_server=\${$ARG2[server_name]}
+            eval local local_db_user=\${$ARG2[db_user]}
+            eval local local_db_pass=\${$ARG2[db_password]}
+            eval local local_db_name=\${$ARG2[db_name]}
+
+            ssh $remote_user@$remote_server "mysqldump -u $remote_db_user -p$remote_db_pass -h $remote_sql_host $remote_db_name" > $backup_file_name
+            mysql -u $local_db_user -p$local_db_pass $local_db_name < $backup_file_name
+
+            check_for_search_replace
+            cp ./Search-Replace-DB-master/srdb.cli.php ./Search-Replace-DB-master/srdb.class.php $local_dir
+
+            ${local_dir}srdb.cli.php -h $local_sql_host -u $local_db_user -p $local_db_pass -n $local_db_name -s "$remote_server" -r "$local_server"
+            rm ${local_dir}srdb.cli.php
+            rm ${local_dir}srdb.class.php
         fi
     else
         echo "Error. No local server."
